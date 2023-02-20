35e72fbef33614f2b1d5670b9ffeca949bec11a1
Konrad Stanik <konrad.stanik@paramount.com>
2023-02-20 16:05:42 +0100

    Bumped KJWT to 0.0.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "io.github.versi.kgcsfetcher"
-version = "0.0.1"
+version = "0.0.2"
 
 repositories {
     mavenCentral()
@@ -84,10 +84,10 @@ kotlin {
                 implementation("org.jetbrains.kotlinx:atomicfu:0.18.5")
                 if (hostOs == "Linux") {
                     implementation("io.github.versi.kurl:kurl:0.0.24")
-                    implementation("com.paramount.kjwt:kjwt:0.0.4")
+                    implementation("com.paramount.kjwt:kjwt:0.0.5")
                 } else {
                     implementation("io.github.versi.kurl:kurl-macosx64:0.0.24")
-                    implementation("com.paramount.kjwt:kjwt-macosx64:0.0.4")
+                    implementation("com.paramount.kjwt:kjwt-macosx64:0.0.5")
                 }
             }
         }
